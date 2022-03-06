eaf7b07d9e96edcd64d198acc5a2b872f259a1fe
Jiang Wu <john.x.wu@ericsson.com>
2022-03-06 08:56:59 +0200

    How to control paralle threads

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module base
 
 go 1.17
+
+require golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9
---
+++ test/thread_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"strconv"
+	"sync"
+	"testing"
+	. "time"
+)
+
+var wg sync.WaitGroup
+var batchCh = make(chan struct{}, 10)
+
+func run(str string) {
+	defer wg.Done()
+	//写到10个时阻塞
+	batchCh <- struct{}{}
+	fmt.Printf("%v %s \n", unix.Gettid(), str)
+	Sleep(Second)
+	<-batchCh
+}
+
+func TestParallel(t *testing.T) {
+	for i := 0; i < 10000; i++ {
+		//t.Skip("Skipping testing")
+		wg.Add(1)
+		go run("Runner No." + strconv.Itoa(i) + " finished")
+	}
+	wg.Wait()
+}
+
+func TestWaitGroup(t *testing.T) {
+	var wgp sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wgp.Add(1)
+		go func(i int) {
+			defer wgp.Done()
+			fmt.Println("Runner No.", strconv.Itoa(i), " finished")
+			Sleep(Second)
+		}(i)
+	}
+	wgp.Wait()
+}
