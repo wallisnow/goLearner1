4fd8351509d73c0d1b2283ccd29aed39b6d9d288
Jiang Wu <john.x.wu@ericsson.com>
2022-03-08 20:54:40 +0200

    Defer for chain caller and in loop

--- test/defer_in_function_test.go
+++ test/defer_in_function_test.go
@@ -5,6 +5,34 @@ import (
 	"testing"
 )
 
+type Student struct{}
+
+func NewStudent() *Student {
+	return &Student{}
+}
+
+func (this *Student) change(i int) *Student {
+	fmt.Println(i)
+	return this
+}
+
+//链式调用中, defer只会在最后一个生效
+//1
+//2
+//3
+//10
+//4
+func TestDeferInChain(t *testing.T) {
+	student := NewStudent()
+	defer student.
+		change(1).
+		change(2).
+		change(3).
+		change(4)
+
+	student.change(10)
+}
+
 //这里因为, 参数在传入函数时其实已经是确定的值, 所以defer 是不会有任何改变
 func TestDeferStillReturn10(t *testing.T) {
 	i := 10
@@ -21,4 +49,30 @@ func TestDeferStillReturn11(t *testing.T) {
 		fmt.Println(*p)
 	}(p)
 	i++
+}
+
+//此时相当于
+/*func TestDeferInLoopTest(t *testing.T) {
+	i := 0
+	defer func() {
+		fmt.Println(i)
+	}()
+	i = 1
+	defer func() {
+		fmt.Println(i)
+	}()
+	i = 2
+	defer func() {
+		fmt.Println(i)
+	}()
+	i = 3
+}*/
+
+func TestDeferInloop(t *testing.T) {
+	for i := 0; i < 3; i++ {
+		//i := i
+		defer func() {
+			fmt.Println(i)
+		}()
+	}
 }
